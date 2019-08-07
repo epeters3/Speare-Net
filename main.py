71d1a4ad6e0c6f3235e0f426f8a3a7d831d1e0e0
Evan Peterson <evanpeterson17@gmail.com>
2019-08-07 16:46:41 -0600

    Now creating sequences (shakespeare dataset too big for memory currently)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import math
+import re
 
 import numpy as np
 import torch
@@ -8,10 +9,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from parse_text import make_codex, make_labels
+from parse_text import make_codex, make_labels, make_sequences, make_simplified_corpus
 
 
-def load(text_path="t8.shakespeare.txt", codex_path="codex.json", labels_path="labels.pkl"):
+def strip_path_and_ext(file_path):
+    file_ext_regex = r"\.[^\.]+$"
+    corpus_file_name = file_path.split("/")[-1]
+    return re.sub(file_ext_regex, "", corpus_file_name)
+
+
+def load(
+    corpus_path="data/t8.shakespeare.txt", labels_path="data/labels.pkl", seq_len=20
+):
     """
     Loads the pickled labels and returns the one-hot
     encoded pytorch input data.
@@ -19,75 +28,100 @@ def load(text_path="t8.shakespeare.txt", codex_path="codex.json", labels_path="l
 
     # Resolve all needed prerequisite files
 
-    if not os.path.isfile(text_path):
-        raise Exception("No file was found at '{}'".format(text_path))
-    
+    corpus_name = strip_path_and_ext(corpus_path)
+    build_path = f"model_data/{corpus_name}"
+    simplified_corpus_path = f"{build_path}/simple-corpus.txt"
+    sequences_path = f"{build_path}/sequences.txt"
+    codex_path = f"{build_path}/codex.json"
+    labels_path = f"{build_path}/labels.pkl"
+
+    if not os.path.isfile(corpus_path):
+        raise Exception(f"No file was found at '{corpus_path}'")
+
+    if not os.path.isdir(build_path):
+        os.makedirs(build_path)
+
+    if not os.path.isfile(simplified_corpus_path):
+        print(f"Making simplified corpus at '{simplified_corpus_path}'...")
+        make_simplified_corpus(corpus_path, simplified_corpus_path)
+
     if not os.path.isfile(codex_path):
-        print("Making codex at '{}'...".format(codex_path))
-        make_codex(text_path, codex_path)
-    
+        print(f"Making codex at '{codex_path}'...")
+        make_codex(simplified_corpus_path, codex_path)
+
+    if not os.path.isfile(sequences_path):
+        print(f"Making sequences at '{sequences_path}'...")
+        make_sequences(simplified_corpus_path, sequences_path)
+
     if not os.path.isfile(labels_path):
-        print("Making labels at '{}'...".format(labels_path))
-        make_labels(codex_path, text_path, labels_path)
+        print(f"Making labels at '{labels_path}'...")
+        make_labels(codex_path, sequences_path, labels_path, seq_len)
 
     # Load the labels
-    with open(labels_path, 'rb') as rf:
-        print("Loading labels from '{}'...".format(labels_path))
+    with open(labels_path, "rb") as rf:
+        print(f"Loading labels from '{labels_path}'...")
         labels = pickle.load(rf)
+
     num_labels = np.max(labels) + 1
-    n = np.size(labels)
+    num_seqs = np.shape(labels)[0]
 
     # Build and return the onehots
-    one_hots = np.zeros((n, num_labels))
-    one_hots[np.arange(n), labels] = 1
-    torch_one_hots = torch.from_numpy(one_hots).float()
+    one_hots = torch.zeros(num_seqs, seq_len, num_labels)
+    for i in range(num_seqs):
+        for j in range(seq_len):
+            char_label = labels[i][j]
+            one_hots[i][j][char_label] = 1
 
     if torch.cuda.is_available():
-        return torch_one_hots.cuda()
-    return torch_one_hots
+        return one_hots.cuda()
+    return one_hots, build_path
+
 
 class SpeareNet(nn.Module):
     """A char-net"""
 
     def __init__(self, in_size, h_size=200):
         super(SpeareNet, self).__init__()
-        self.a_gru = nn.GRU(in_size, h_size, num_layers=2)
-        self.b_dense = nn.Linear(h_size, in_size)
-    
-    def forward(self, x):
-        h1, _ = self.a_gru(x)
-        return F.softmax(self.b_dense(h1), dim=2)
+        self.in_size = in_size
+        self.h_size = h_size
+        self.gru = nn.GRU(in_size + h_size, h_size, num_layers=2)
+        self.dense = nn.Linear(h_size, in_size)
+
+    def forward(self, x, hidden):
+        h1, _ = self.gru(torch.cat((x, hidden)))
+        return F.softmax(self.dense(h1), dim=2)
+
+    def get_init_h(self):
+        return torch.zeros(self.h_size)
 
 
-def train(data, *, epochs=100, report_every=100, checkpoint_every=10, seq_size=50, save_dir="checkpoints"):
-    n, c = data.size()
-    model = SpeareNet(c)
+def train(data, build_path, *, epochs=100, report_every=100, checkpoint_every=10):
+    num_seqs, seq_len, input_len = data.size()
+    model = SpeareNet(input_len)
     if torch.cuda.is_available():
         model = model.cuda()
     params = model.parameters()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(params, 1e-4)
 
-    num_iters = n - seq_size
     sum_loss = 0.0
     num_correct = 0.0
-    for epoch_i in range(1, epochs+1):
+    for epoch_i in range(1, epochs + 1):
         iters_done = 0
-        for data_i in range(num_iters):
+        for seq_i in np.random.permutation(num_seqs):
 
-            target_i = data_i + seq_size
             model.zero_grad()
 
             # Forward pass through this character sequence
-            outputs = None
-            for seq_i in range(data_i, target_i):
-                inputs = data[seq_i, :].view(1, 1, -1)
-                outputs = model(inputs)
+            outputs = model.get_init_h()
+            for char_i in range(seq_len - 1):
+                inputs = data[seq_i, char_i, :].view(1, 1, -1)
+                outputs = model(inputs, outputs)
 
             # Backpropagate through time. We are trying to
             # predict the character right after the input sequence,
             # the one-hot encoded character vector at `target_i`.
-            target = data[target_i, :].view(1, -1).long()
+            target = data[seq_i, -1, :].view(1, -1).long()
             _, target_label = target.max(dim=1)
             outputs = outputs.view(1, -1)
             _, output_label = outputs.max(dim=1)
@@ -107,21 +141,19 @@ def train(data, *, epochs=100, report_every=100, checkpoint_every=10, seq_size=5
                 sum_loss = 0.0
                 num_correct = 0.0
                 print(
-                    f"[{round(iters_done/num_iters*100, 4)}% of epoch {epoch_i}]:"
+                    f"[{round(iters_done/num_seqs*100, 4)}% of epoch {epoch_i}]:"
                     f"\n\tavg_loss = {avg_loss}"
                     f"\n\tavg_accuracy = {avg_accuracy}"
                 )
 
         # Checkpoint the model after `checkpoint_every` epochs
         if epoch_i % checkpoint_every == 0:
-            if not os.path.isdir(save_dir):
-                os.mkdir(save_dir)
-            torch.save(model.state_dict(), f"{save_dir}/epoch_{epoch_i}.pt")
+            torch.save(model.state_dict(), f"{build_path}/epoch_{epoch_i}.pt")
 
 
 if __name__ == "__main__":
-    data = load(text_path="sixpence_small.txt", )
-    train(data, epochs=400, seq_size=20, report_every=170)
+    data, build_path = load(corpus_path="datasets/sixpence.txt")
+    train(data, build_path, epochs=400, report_every=170)
 
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
--- parse_text.py
+++ parse_text.py
@@ -2,19 +2,65 @@ import json
 import numpy as np
 import pickle
 
-def make_codex(text_path="t8.shakespeare.txt", write_path="codex.json"):
+
+def make_simplified_corpus(
+    text_path="data/t8.shakespeare.txt",
+    write_path="data/simple-corpus.txt",
+    remove=["\n"],
+    to_lower=True,
+):
+    """
+    Eliminate all tokens in `remove` from the file at
+    `text_path`, also optionally converting the file to lower
+    case too.
+    """
+    print(f"Simplifying '{text_path}'...")
+    with open(text_path, "r") as rf:
+        with open(write_path, "w") as wf:
+            for corpus_line in rf.readlines():
+                for token in remove:
+                    corpus_line = corpus_line.replace(token, "")
+                # Add a space at the end in case the next line
+                # doesn't begin with a space.
+                corpus_line += " "
+                if to_lower:
+                    corpus_line = corpus_line.lower()
+                wf.write(corpus_line)
+    print(f"Results written to '{write_path}'")
+
+
+def make_sequences(
+    text_path="data/simple-corpus.txt", write_path="data/sequences.txt", seq_len=20
+):
+    print(f"Constructing sequences of length {seq_len} from '{text_path}'")
+    current_seq = ""
+    with open(text_path, "r") as rf:
+        with open(write_path, "w") as wf:
+            for line in rf:
+                for char in line:
+                    # Append this character to the end of `current_seq`
+                    current_seq += char
+                    if len(current_seq) > seq_len:
+                        # Trim to keep just the last `seq_len`
+                        # characters of `current_seq`.
+                        current_seq = current_seq[-seq_len:]
+                    if len(current_seq) == seq_len:
+                        wf.write(f"{current_seq}\n")
+
+
+def make_codex(text_path="data/simple-corpus.txt", write_path="data/codex.json"):
     """
     Parses the file at `text_path`, finding all unique
     characters in the file, and encoding them in a vector.
     Each char's code will be the index of its
     position in `codex`, the array that is written to
-    `write_path` as JSON. 
+    `write_path` as JSON.
     """
     codex = []
     char_cnt = 0
     token_cnt = 0
-    print("Encoding document: '{}'...".format(text_path))
-    with open(text_path, 'r') as rf:
+    print(f"Encoding document: '{text_path}'...")
+    with open(text_path, "r") as rf:
         for line in rf:
             for char in line:
                 char_cnt += 1
@@ -22,42 +68,46 @@ def make_codex(text_path="t8.shakespeare.txt", write_path="codex.json"):
                     token_cnt += 1
                     codex.append(char)
 
-    with open(write_path, 'w') as wf:
-        print("Writing codex to document: '{}'...".format(write_path))
-        json.dump({
-            "char_cnt": char_cnt,
-            "token_cnt": token_cnt,
-            "codex": codex
-        }, wf)
+    with open(write_path, "w") as wf:
+        print(f"Writing codex to document: '{write_path}'...")
+        json.dump({"char_cnt": char_cnt, "token_cnt": token_cnt, "codex": codex}, wf)
 
-def make_labels(codex_path="codex.json", text_path="t8.shakespeare.txt", labels_path="labels.pkl"):
+
+def make_labels(
+    codex_path="data/codex.json",
+    sequences_path="data/t8.shakespeare.txt",
+    labels_path="data/labels.pkl",
+    seq_len=20,
+):
     """
     Uses the codex at `codex_path` (stored as JSON), and
-    the text at `text_path`, and builds a numpy array
-    that holds the encoded integers for each char in
-    the document. Can be used to make a one-hot matrix.
+    the text at `sequences_path`, and builds a numpy matrix
+    that holds the encoded integers for each sequence in
+    the document.
     """
-    with open(codex_path, 'r') as rmf:
-        print("Reading codex data: '{}'...".format(codex_path))
+    with open(codex_path, "r") as rmf:
+        print(f"Reading codex data: '{codex_path}'...")
         text_meta = json.load(rmf)
-    char_cnt = text_meta['char_cnt']
-    token_cnt = text_meta['token_cnt']
-    codex = text_meta['codex']
+    char_cnt = text_meta["char_cnt"]
+    token_cnt = text_meta["token_cnt"]
+    codex = text_meta["codex"]
 
-    labels = np.zeros((char_cnt), dtype=np.int32)
-    
+    labels = np.zeros((char_cnt, seq_len), dtype=np.int32)
+
     # Populate the labels
-    char_idx = 0
-    with open(text_path, 'r') as rtf:
-        print("Reading text file: '{}'...".format(text_path))
+    seq_idx = 0
+    with open(sequences_path, "r") as rtf:
+        print(f"Reading text file: '{sequences_path}'...")
         for line in rtf:
-            for char in line:
+            # Don't consider the last char of line here
+            # since it is always '\n'
+            for char_idx, char in enumerate(line[:-1]):
                 # Use the integer encoding of this char
                 # as its label.
-                labels[char_idx] = codex.index(char)
-                char_idx += 1
-    
+                labels[seq_idx, char_idx] = codex.index(char)
+            seq_idx += 1
+
     # Write out the labels
-    with open(labels_path, 'wb') as wf:
-        print("Writing labels data to: '{}'...".format(labels_path))
+    with open(labels_path, "wb") as wf:
+        print(f"Writing labels data to: '{labels_path}'...")
         pickle.dump(labels, wf)
